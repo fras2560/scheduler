f2b455ac6a4ed0745d36abec1649871998c3d9a0
fras2560 <dallas.fraser.waterloo@gmail.com>
2024-01-03 21:43:48 -0500

    Add parameter for replace current state
    Along with two tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,8 +3,13 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]); 
-  function transition(mode) {
-    setHistory((prev) => [...prev, mode]);
+  function transition(mode, replace = false) {
+    if (replace) {
+      setHistory((prev) => [...prev.slice(0, -1), mode]);
+    } else {
+      setHistory((prev) => [...prev, mode]);
+    }
+    
   }
   function back() {
     if (history.length > 1) {
