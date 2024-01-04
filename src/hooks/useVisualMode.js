074433b1d13ef5db57c1496735f8db05f5564771
fras2560 <dallas.fraser.waterloo@gmail.com>
2024-01-03 21:38:32 -0500

    Add history of modes to custom hook
    Along with a test for back functionality

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,9 +2,18 @@ import { useState } from "react";
 
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); 
+  function transition(mode) {
+    setHistory((prev) => [...prev, mode]);
+  }
+  function back() {
+    if (history.length > 1) {
+      setHistory((prev) => [...prev.slice(0, -1)]);
+    }
+  }
   return {
-    mode: mode,
-    transition: setMode
+    mode: history[history.length - 1],
+    transition: transition,
+    back: back
   };
 };
